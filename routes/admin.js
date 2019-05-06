845bbb87b2f35132d0694b82fb8d017cc669898a
kumudug <kumudug1@gmail.com>
2019-05-05 17:53:05 -0700

    add a layout page

--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,7 @@ const products = [];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render('add-product', { prods: products, pageTitle: 'Add Product' });
+  res.render('add-product', { prods: products, pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
 // /admin/add-product => POST
